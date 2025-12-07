303c0581953946190558cecbe5ffe191fd7abd20
alyannce <alyanncevhon@gmail.com>
2025-12-08 04:59:39 +0800

    Added UI and some few questions for testing

---
+++ lib/data/questions.dart
@@ -0,0 +1,19 @@
+import '../models/question.dart';
+
+final List<Question> questions = [
+  Question(
+    question: "What language does Flutter use?",
+    options: ["Java", "Kotlin", "Dart", "Python"],
+    correctIndex: 2,
+  ),
+  Question(
+    question: "What widget is used for layouts?",
+    options: ["Text", "Column", "Icon", "Scaffold"],
+    correctIndex: 1,
+  ),
+  Question(
+    question: "Who developed Flutter?",
+    options: ["Apple", "Microsoft", "Google", "Meta"],
+    correctIndex: 2,
+  ),
+];
